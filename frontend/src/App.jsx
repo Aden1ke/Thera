6be46ca48ac5912f01f2634cade9416e841f485b
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2025-05-28 16:16:17 +0100

    feat:added the base foundation for styling

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/home/header";
+import { SoulTracker } from "./components/home/soul";
+
+function App() {
+  return (
+    <section className="max-w-screen-xl w-full mx-auto">
+      <Header />
+      <SoulTracker />
+    </section>
+  );
+}
+
+export default App;
